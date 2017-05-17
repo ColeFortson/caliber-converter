ea6278e80bc3b83c56532c6d04cee81d946d7941
Philip Taylor <philiptaylor13@gmail.com>
2017-05-17 15:26:36 -0500

    fixed looping with ffmpeg concat

--- caliber.py
+++ caliber.py
@@ -8,6 +8,8 @@
 
 import os, glob, shutil
 from shutil import copyfile
+from subprocess import call
+
 
 prefix = "cimg_"
 fileType = "png"
@@ -44,9 +46,9 @@ outputName += ".mp4"
 
 
 for i in range(numGroups):
-    
+
     dir = ("caliber_shot_" + str(i))
-    
+
     if os.path.exists(dir):
         shutil.rmtree(dir)
 # Create directory if it doesn't already exist
@@ -63,17 +65,16 @@ for i in range(numGroups):
     shutil.copy2(prefix + str(i) + "_" + str(2) + ".png", dir + "/" + prefix + str(i) + "_" + str(4) + ".png")
     shutil.copy2(prefix + str(i) + "_" + str(1) + ".png", dir + "/" + prefix + str(i) + "_" + str(5) + ".png")
 
-# Don't even ask idk either anymore lol. it works so don't fuck with it
+    # Don't even ask idk either anymore lol. it works so don't fuck with it
     os.system("ffmpeg -f image2 -r 12 -i " + dir + "/cimg_" + str(i) + "_%d.png -vcodec mpeg4 -y " + dir + "/" + str(outputName))
     f = open(dir + "/cat.txt","w+")
-    f.write("# meow\r\n")
     for i in range(4):
-        f.write("file '%s'\r\n" % (outputName))
+        f.write("file '%s'\n" % (outputName))
+    f.close()
 
-#    os.system("ffmpeg -i " + dir + "/" + str(outputName) + " -c copy -bsf:v h264 -f mpegts intermediate.ts")
-#    os.system("ffmpeg -i 'concat:intermediate1.ts|intermediate1.ts' -c copy -bsf:a aac_adtstoasc " + dir + "/" + "output.mp4")
-
-
+    command="ffmpeg -safe 0 -f concat -i %s/cat.txt -c copy %s/looped.mp4" %(dir,dir)
+    os.system(command)
+    
 
 # "IF I CAN'T BE THE BEST, I SURE AS HELL CAN BE THE WOOOOOORSSSTTTTTTT!" - Jon Jafari ( https://www.youtube.com/watch?v=RSKmPP7ckzU )
 # Stay inspired <3
